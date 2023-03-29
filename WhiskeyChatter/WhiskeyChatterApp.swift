39eba6d0c5833ef1471f63dc25f1004c7a726ad4
Ursta Web Solutions <info@urstawebsolutions.com>
2023-03-29 06:03:16 -0400

    Initial Commit

---
+++ WhiskeyChatter/WhiskeyChatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiskeyChatterApp.swift
+//  WhiskeyChatter
+//
+//  Created by Michael Ursta on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WhiskeyChatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
