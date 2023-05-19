fe96684d8876d8cfdb48d93fb7d576969546bf3b
Ursta Web Solutions <info@urstawebsolutions.com>
2023-05-19 15:59:25 -0400

    post work

--- WhiskeyChatter/Views/Interaction/WCNewPost.swift
+++ WhiskeyChatter/Views/Interaction/WCNewPost.swift
@@ -21,86 +21,116 @@ struct WCNewPost: View {
     @State private var showImagePicker: Bool = false
     @FocusState private var showKeyboard: Bool
    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 0){
-            HStack{
-                Menu {
-                    Button("Cancel", role: .destructive){
-                        dismiss()
-                    }
-                }label:{
-                    Text("Cancel")
-                        .font(Font.menuAction)
-                        .foregroundColor(.white)
+var body: some View {
+    VStack(alignment: .leading, spacing: 0){
+        HStack{
+            Menu {
+                Button("Cancel", role: .destructive){
+                    dismiss()
                 }
-                Spacer()
-                Text("CHATTER")
+            }label:{
+                Text("Cancel")
+                    .font(Font.menuAction)
                     .foregroundColor(.white)
-                    .font(Font.header)
-                Spacer()
-                Button(action: {}){
-                    Text("Post")
-                        .font(Font.menuAction)
-                        .foregroundColor(.white)
-                }
-                .disabled(commentText == "")
             }
-            .padding(.horizontal,15)
-            .padding(.vertical,3)
-            .background(Color.black)
-        
-        ScrollView(.vertical, showsIndicators: false){
-            VStack(spacing: 15){
-                TextField("Tell us about your drink!", text: $commentText, axis: .vertical)
-                    .focused($showKeyboard)
-                
-                if let commentImageData, let image = UIImage(data: commentImageData){
-                    GeometryReader{
-                        let size = $0.size
-                        Image(uiImage: image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: size.width, height: size.height)
-                            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                        }
-                        .clipped()
-                        .frame(height:220)
-                    }
-                }
-                .padding(20)
+            Spacer()
+            Text("CHATTER")
+                .foregroundColor(.white)
+                .font(Font.header)
+            Spacer()
+            Button(action: preparetPost){
+                Text("Post")
+                    .font(Font.menuAction)
+                    .foregroundColor(.white)
             }
-            Divider()
-            HStack{
-                Button{
-                    showImagePicker.toggle()
-                }label:{
-                    Image(systemName: "photo.on.rectangle")
-                }
-                Spacer()
-                Button("Done"){
-                    showKeyboard = false
-                }
-            }
-            .padding(.horizontal,15)
-            .padding(.vertical,5)
-            .padding(.horizontal,15)
-            .foregroundColor(Color.black)
+            .disabled(commentText == "")
         }
-        .photosPicker(isPresented: $showImagePicker, selection: $photoItem)
-        .onChange(of: photoItem){photo in
-            if let photo{
-                Task{
-                    if let imageData = try? await photo.loadTransferable(type: Data.self),
-                       let image = UIImage(data: imageData),
-                       let compressedImageData = image.jpegData(compressionQuality: 0.5){
-                            await MainActor.run(body:{
-                                commentImageData = compressedImageData
-                                photoItem = nil
-                            })
+        .padding(.horizontal,15)
+        .padding(.vertical,3)
+        .background(Color.black)
+        
+    ScrollView(.vertical, showsIndicators: false){
+        VStack(spacing: 15){
+            TextField("Tell us about your drink!", text: $commentText, axis: .vertical)
+                .focused($showKeyboard)
+            
+            if let commentImageData, let image = UIImage(data: commentImageData){
+                GeometryReader{
+                    let size = $0.size
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: size.width, height: size.height)
+                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                        .overlay(alignment: .topTrailing){
+                            Button{
+                                withAnimation(.easeInOut(duration:0.25)){
+                                    self.commentImageData = nil
+                                }
+                            }label:{
+                                Image(systemName: "trash")
+                                    .fontWeight(.bold)
+                                    .tint(.white)
+                                    .background(.black)
+                            }
+                            .padding(10)
                         }
+                    }
+                    .clipped()
+                    .frame(height:220)
+                }
+            }
+            .padding(20)
+        }
+        Divider()
+        HStack{
+            Button{
+                showImagePicker.toggle()
+            }label:{
+                Image(systemName: "photo.on.rectangle")
+            }
+            Spacer()
+            Button("Done"){
+                showKeyboard = false
+            }
+        }
+        .padding(.horizontal,15)
+        .padding(.vertical,5)
+        .padding(.horizontal,15)
+        .foregroundColor(Color.black)
+    }
+    .photosPicker(isPresented: $showImagePicker, selection: $photoItem)
+    .onChange(of: photoItem){photo in
+        if let photo{
+            Task{
+                if let imageData = try? await photo.loadTransferable(type: Data.self),
+                    let image = UIImage(data: imageData),
+                    let compressedImageData = image.jpegData(compressionQuality: 0.5){
+                        await MainActor.run(body:{
+                            commentImageData = compressedImageData
+                            photoItem = nil
+                        })
                     }
                 }
             }
+        }
+    }
+    
+    func preparetPost(){
+        isLoading = true
+        showKeyboard = false
+        let currentUser = UserService.sharedUser.user
+        
+        Task{
+            do{
+                guard let profileURL = profileURL else{return}
+                let imageRefID = "\(currentUser.userId)\(Date())"
+                let storageRef = Storage.storage().reference().child("Post_Images").child
+            }
+            catch{
+                await setError(error)
+            }
+        }
     }
 }
 
